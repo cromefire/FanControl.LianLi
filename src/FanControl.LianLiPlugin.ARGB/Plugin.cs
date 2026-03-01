9facba83ef65480c0a5669ab7826a03a37224afa
Cromefire_ <cromefire.supernova559@passmail.com>
2026-03-01 16:16:42 +0100

    Migrate to .NET 10 and sane project structure

---
+++ src/FanControl.LianLiPlugin.ARGB/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using FanControl.LianLiPlugin.Base;
+using FanControl.Plugins;
+
+namespace FanControl.LianLiPlugin;
+
+public class LianLiPlugin : IPlugin
+{
+    private LianLiPluginBase pluginBase = new(true);
+    public string Name => "Lian Li ARGB";
+
+    public void Close()
+    {
+        pluginBase.Close();
+    }
+
+    public void Initialize()
+    {
+        pluginBase.Initialize();
+    }
+
+    public void Load(IPluginSensorsContainer _container)
+    {
+        pluginBase.Load(_container);
+    }
+}
---
+++ src/FanControl.LianLiPlugin.Base/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using FanControl.Plugins;
+
+namespace FanControl.LianLiPlugin.Base;
+
+public class LianLiPluginBase(bool enableArgb)
+{
+    private LianLi.Devices _devices;
+    private bool _initialized;
+
+    public void Close()
+    {
+        // Close all Lian Li Devices
+        _devices.Dispose();
+    }
+
+    public void Initialize()
+    {
+        _devices = new LianLi.Devices(enableArgb);
+        _initialized = _devices.FanControllers_Located();
+    }
+
+    public void Load(IPluginSensorsContainer _container)
+    {
+        if(_initialized)
+        {
+            /** Load Fan Controllers **/
+            // Each Controller consists of 4 Channels - Configure a sensor for each  
+            var fanControlSensorsCount = _devices.FanControllers_Count() * 4;
+            for (var i = 0; i < fanControlSensorsCount; i++)
+            {
+                var controllerIndex = i / 4;
+                var channelIndex = i % 4;
+                _container.ControlSensors.Add(new ControlSensors(_devices, controllerIndex, channelIndex));
+                _container.FanSensors.Add(new Sensors(_devices, controllerIndex, channelIndex));
+            }
+
+        }
+    }
+}
